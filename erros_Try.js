d0d00bed1fe0850064104744d6a7608291fda215
rodrigo33 <rbarbosa.barbosa@gmail.com>
2020-11-16 15:58:23 -0300

    Utilizando classe error e o construtor no tratamento

--- erros_Try.js
+++ erros_Try.js
@@ -1,13 +1,26 @@
+class CustomError extends Error {
+    constructor({message, data}){
+        super(message)
+        this.data = data
+    }
+}
 
-try{
-  
+try{  
     const name = 'rodrigo barbosa'
 
-    const myError = new Error('custo message')
+    const myError = new CustomError({
+        message: 'message custom error',
+        data:{
+            type:'server error'
+        }
+    })
     throw myError
 }   catch (err){
-    console.log('Error:', err)
+    if (err.data.type === 'server error'){        
+    }else{
+
+    }    
 }   finally{
-    console.log('keep going')
+    console.log('keep going..')
 }
 
